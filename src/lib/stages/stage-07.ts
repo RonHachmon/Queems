295c11db9da3dc22dd059a09ea6ccb029263c6ac
Ron H <hachmonron@gmail.com>
2026-02-18 22:10:47 +0200

    import stage

---
+++ src/lib/stages/stage-07.ts
@@ -0,0 +1,18 @@
+import type { Stage } from '@/types'
+
+
+export const stage07: Stage = {
+  id: 'stage-07',
+  number: 6,
+  label: '18/02',
+  size: 7,
+  grid: [
+    ['purple', 'purple', 'purple', 'purple', 'orange', 'orange', 'blue'  ],
+    ['purple', 'green' , 'green' , 'green' , 'green' , 'green' , 'blue'  ],
+    ['purple', 'green' , 'grey'  , 'red'   , 'red'   , 'green' , 'blue'  ],
+    ['purple', 'green' , 'grey'  , 'red'   , 'grey'  , 'green' , 'yellow'],
+    ['purple', 'green' , 'grey'  , 'grey'  , 'grey'  , 'green' , 'yellow'],
+    ['purple', 'green' , 'green' , 'green' , 'green' , 'green' , 'yellow'],
+    ['yellow', 'yellow', 'yellow', 'yellow', 'yellow', 'yellow', 'yellow']
+  ],
+}
